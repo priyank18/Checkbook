48d9f97d9656b5492f3a52fcdb4ec475a8be03dd
priyank18 <priyank18@outlook.com>
2015-05-12 19:15:59 -0400

    Initialize project

---
+++ WpfApplication2/CheckBookVM.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using System.Collections.ObjectModel;
+
+namespace CheckBook
+{
+    public class CheckBookVM : BaseVM
+    {
+        public CheckBookVM()
+        {
+        }
+
+        CbDb _Db = new CbDb();
+
+        private int _RowsPerPage = 5;
+        private int _CurrentPage = 1;
+        public int CurrentPage
+        {
+            get { return _CurrentPage; }
+            set { _CurrentPage = value; OnPropertyChanged(); OnPropertyChanged("CurrentTransactions"); }
+        }
+
+        private ObservableCollection<Transaction> _Transactions;
+        public ObservableCollection<Transaction> Transactions
+        {
+            get { return _Transactions; }
+            set { _Transactions = value; OnPropertyChanged(); OnPropertyChanged("Accounts"); }
+        }
+
+        public IEnumerable<Account> Accounts
+        {
+            get { return _Db.Accounts.Local; }
+        }
+
+        public IEnumerable<Transaction> CurrentTransactions
+        {
+            get { return Transactions.Skip((_CurrentPage - 1) * _RowsPerPage).Take(_RowsPerPage); }
+        }
+
+        public DelegateCommand MoveNext
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    ExecuteFunction = _ => CurrentPage++,
+                    CanExecuteFunction = _ => CurrentPage * _RowsPerPage < Transactions.Count
+                };
+            }
+        }
+
+        public DelegateCommand Save
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    ExecuteFunction = _ => _Db.SaveChanges(),
+                    CanExecuteFunction = _ => _Db.ChangeTracker.HasChanges()
+                };
+            }
+        }
+
+        public DelegateCommand NewTransaction
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    ExecuteFunction = _ =>
+                    {
+                        Transactions.Add(new Transaction { });
+                        CurrentPage = Transactions.Count / _RowsPerPage + 1;
+                    }
+                };
+            }
+        }
+
+       private string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; OnPropertyChanged(); }
+        }
+        private string _Email;
+
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; OnPropertyChanged(); }
+        }
+        private string _Picture;
+
+        public string Picture
+        {
+            get { return _Picture; }
+            set { _Picture = value; OnPropertyChanged(); }
+        }
+
+        public void Fill()
+        {
+            Transactions = _Db.Transactions.Local;
+            _Db.Accounts.ToList();
+            _Db.Transactions.ToList();
+
+        }
+    }
+}
